1a8e7634d986c757865550fe9570ff9c5fc41261
Victor Cesconetto De Pieri <victorcesc@users.noreply.github.com>
2019-09-11 22:09:15 -0300

    Update Trab2_com1_victor.m

--- Trab2_com1_victor.m
+++ Trab2_com1_victor.m
@@ -141,20 +141,84 @@ xlim([-20e3 20e3])
 
 %soma as componentes filtradas
 
-sinal = Sf1+Sf2+Sf3;
-
 
 
 %demodula cada componente pela portadora original
 
+figure(6)
+sinal_f = Sf1+Sf2+Sf3;
+plot(freq,abs(sinal_f))
 
+
+s_t1 = ifft(Sf1).*ct1;
+s_t2 = ifft(Sf2).*ct2;
+s_t3 = ifft(Sf3).*ct3;
+
+Sf1 = fft(s_t1);
+Sf2 = fft(s_t2);
+Sf3 = fft(s_t3);
+SFfinal = Sf1+Sf2+Sf3;
+
+figure(7)
+subplot(411)
+plot(freq,abs(Sf1))
+subplot(412)
+plot(freq,abs(Sf2))
+subplot(413)
+plot(freq,abs(Sf3))
+subplot(414)
+plot(freq,abs(SFfinal))
 
 
 %passa baixa para recuperar o sinal original
 
+filtro_PB1 = [zeros(1,96000) ones(1,8000) zeros(1,96001)];
+Sf_filtrado = filtro_PB1.*SFfinal;
+
+figure(8)
+subplot(211)
+plot(freq,abs(Sf_filtrado))
 
 
+St_filtrado = ifft(Sf_filtrado);
+subplot(212)
+plot(t,St_filtrado)
 
+%passa faixa para separar os sinais originais
 
+filtro_PF1 = [zeros(1,98500)  ones(1,1000) zeros(1,1000) ones(1,1000)  zeros(1,98501)];
+filtro_PF2 = [zeros(1,97500) ones(1,1000) zeros(1,3000) ones(1,1000) zeros(1,97501)];
+filtro_PF3 = [zeros(1,96500) ones(1,1000) zeros(1,5000) ones(1,1000) zeros(1,96501)];
 
+figure(9)
+plot(freq,filtro_PF1)
+
+mf1 = Sf_filtrado.*filtro_PF1;
+
+mt1 = ifft(mf1);
+subplot(611)
+plot(t,mt1)
+xlim([0 0.01])
+subplot(612)
+plot(freq,abs(mf1))
+
+mf2 = Sf_filtrado.*filtro_PF2;
+
+mt2 = ifft(mf2);
+
+subplot(613)
+plot(t,mt2)
+xlim([0 0.01])
+subplot(614)
+plot(freq,abs(mf2))
+
+mf3 = Sf_filtrado.*filtro_PF3;
+
+mt3 = ifft(mf3);
+
+subplot(615)
+plot(t,mt3)
+xlim([0 0.01])
+subplot(616)
+plot(freq,abs(mf3))
  
