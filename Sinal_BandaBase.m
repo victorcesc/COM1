072649a17a7e29057ba4aa1f3f8c72c7fcea8f13
Victor Cesconetto De Pieri <victorcesc@users.noreply.github.com>
2019-09-16 17:17:38 -0300

    adicionado taxa de erro e o sinal recebido - receptor

--- Sinal_BandaBase.m
+++ Sinal_BandaBase.m
@@ -3,11 +3,11 @@ close all
 clc
 
 N = 100;
-M = 4; %niveis de transmissao(ex 2 bits(0 a 3 decimal) = 4 niveis, 3bits(0 a 7 decimal) = 8 niveis)
-l = log2(M); %qtd de bits p/ nivel de transmissao
-A = 3; %amplitude maxima - para aumentar a amplitude precisa-se fazer um calculo com relação a simetria dos niveis
+M = 2; %niveis de transmissao(ex 2 bits(0 a 3 decimal) = 4 niveis, 3bits(0 a 7 decimal) = 8 niveis)
+l = log2(M); %qtd de bits p/ nivel de transmissao - Rb = Rs*log2(M)
+A = 1; %amplitude maxima - para aumentar a amplitude precisa-se fazer um calculo com relação a simetria dos niveis
 dist_nivel = 2; %aqui podemos fazer uma divisao por 2 e pegar o resto pra verificar se o valor é simetrico em relação aos niveis.
-num_simb = 5e2;
+num_simb = 10000;
 info_bin = randint(1,(num_simb*l));
 info_bin = transpose(reshape(info_bin, l,num_simb));
 
@@ -28,11 +28,12 @@ subplot(211)
 stem(info_tx)
 ylim([0 2])
 
+
 subplot(212)
 plot(info_tx)
 xlim([0 10*N])
 
-v = 0.01; %quanto maior a variancia maior o ruido
+v = 1.65; %quanto maior a variancia maior o ruido
 %adicionando ruido
 ruido = v.*randn(length(info_tx),1); %variancia*rndn + media;
 figure(3)
@@ -42,12 +43,28 @@ hist(ruido,100)
 info_rx = info_tx + ruido;
 figure(4)
 plot(info_rx)
-xlim([0 1000])
+xlim([0 10*N])
 
+
+
+% receptor
+limiar = 0;
+figure(5)
+info_hat = info_rx(N/2:N:end) > limiar;
+stem(info_hat)
+
+%taxa de erro
+
+num_erro = sum(xor(info_bin,info_hat))
+taxa_erro = num_erro/length(info_bin)
+
+
+
+
+%% gaussiana
 %media
 mean(ruido)
 %variancia
 var(ruido)
 %desvio padrao
 std(ruido)
-
