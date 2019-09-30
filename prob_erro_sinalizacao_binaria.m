1e98fdde24f0b05f491416a3803ddc3bf860bd26
Victor Cesconetto De Pieri <victorcesc@users.noreply.github.com>
2019-09-30 15:52:43 -0300

    Create prob_erro_sinalizacao_binaria.m

---
+++ prob_erro_sinalizacao_binaria.m
@@ -0,0 +1,22 @@
+
+close all;
+clear all;
+clc;
+
+A = 1;
+T = 10;
+
+Eb_No_dB = 0:15;
+Eb_No = db2pow(Eb_No_dB);
+
+Pb_unipolar = qfunc(sqrt(Eb_No));
+Pb_bipolar = qfunc(sqrt(2*Eb_No));
+
+figure,
+semilogy(Eb_No_dB, Pb_unipolar);hold on; grid on;
+semilogy(Eb_No_dB, Pb_bipolar);
+legend('Sinalização Unipolar', 'Sinalização Bipolar')
+title('Probabilidade de erro para sinalização binária')
+xlabel('Eb/No [dB]')
+ylabel('Pb')
+ylim([1e-7 1])
